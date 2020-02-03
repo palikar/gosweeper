1bb1ce77f3347492f1c2ad90b909e76ddbe55e88
Stanislav Arnaudov <stanislav_ts@abv.bg>
2020-02-03 12:31:36 +0100

    Addin initualization of the grid

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fyne.io/fyne"
 
 	"fyne.io/fyne/app"
+	"math/rand"
 	// "fyne.io/fyne/canvas"
 	// "image/color"
 	"fyne.io/fyne/layout"
@@ -14,29 +15,84 @@ import (
 
 // MineCell ...
 type MineCell struct {
-	hasMine bool
-	hasFlag bool
-	btn     *widget.Button
+	hasMine            bool
+	hasFlag            bool
+	opened             bool
+	neighbourMineCount int
+	btn                *widget.Button
 }
 
+var buttonGrid [20][20]MineCell
+
+// NewCell ...
 func NewCell(btn *widget.Button) MineCell {
 	cell := MineCell{}
 	cell.hasMine = false
 	cell.hasFlag = false
+	cell.opened = false
+	cell.neighbourMineCount = 0
 	return cell
 }
 
 // initGrid ...
 func initGrid() {
 
-}
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 20; j++ {
+			buttonGrid[i][j] = NewCell(nil)
+		}
+	}
 
-var buttonGrid [20][20]MineCell
+	for i := 0; i < 20; i++ {
+		rX := rand.Int31n(20)
+		rY := rand.Int31n(20)
+		buttonGrid[rX][rY].hasMine = true
+		// fmt.Printf("%d:%d\n", r_x, r_y)
+	}
+
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 20; j++ {
+
+			for dX := -1; dX <= 1; dX++ {
+				for dY := -1; dY <= 1; dY++ {
+					x := i + dX
+					y := i + dY
+
+					if x >= 20 {
+						continue
+					}
+					if y >= 20 {
+						continue
+					}
+					if x < 0 {
+						continue
+					}
+					if y < 0 {
+						continue
+					}
+
+					if buttonGrid[x][y].hasMine {
+						buttonGrid[i][j].neighbourMineCount++
+					}
+				}
+			}
+
+		}
+
+	}
+
+}
 
 func clickMine(x int, y int) {
 	fmt.Printf("%s:%d:%d\n", "click", x, y)
 	buttonGrid[x][y].btn.SetText("X")
 	buttonGrid[x][y].btn.Disable()
+
+	if buttonGrid[x][y].hasMine {
+		fmt.Printf("%s\n", "Boom!")
+
+	}
+
 }
 
 func clickFlag(x int, y int) {
@@ -88,6 +144,8 @@ func gameScreen(a fyne.App) fyne.CanvasObject {
 
 func main() {
 	fmt.Println("Starting The Game")
+	initGrid()
+
 	a := app.NewWithID("palikar.go.sweeper")
 	a.SetIcon(theme.FyneLogo())
 	w := a.NewWindow("GoSweeper")
